f946a9140d71735401b7c39f40f4ace7fa124792
aa307359989 <307359989@qq.com>
2019-07-03 14:49:59 +0800

    akalian

--- src/App.js
+++ src/App.js
@@ -1,18 +1,8 @@
 // 老二文件
-<<<<<<< HEAD
-import React, { Component } from 'react';
-import Loadable from 'react-loadable';
-import { Icon } from 'antd';
-import { HashRouter as Router, Route, NavLink, Switch, Redirect } from 'react-router-dom';
-import { HomeWarp, NavWarp, MainWarp } from './styles/style.js';
-
-const Cinema = Loadable({
-=======
 import React, { Component } from 'react'
 import Loadable from 'react-loadable'
 import { HashRouter as Router, Route, Switch, Redirect } from 'react-router-dom'
 const Login = Loadable({
->>>>>>> f77ecb4a4689ee7d134d53cb3984a1c2546f9b87
   // 懒加载
   //需要异步加载的组件
   // 登录
